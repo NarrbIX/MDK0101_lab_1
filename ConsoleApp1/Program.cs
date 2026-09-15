bbd4263d3970365b88a0fc547700baf0ecb498a3
Narrbix <narrbix2705@gmail.com>
2026-09-15 09:25:10 +0300

    Вызываю функции в блоке мейн, проверяю по примеру из задачи, все работает.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-          
+            DeliveryService.CalculateAndShow();
+            Console.ReadKey();
         }
     }
 }
