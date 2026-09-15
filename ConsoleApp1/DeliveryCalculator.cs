85fab621793640220e6af7a6a3aba8a0acacabba
Narrbix <narrbix2705@gmail.com>
2026-09-15 09:22:21 +0300

    Прописал логику ввода для заказа, растояния и время заказа.

--- ConsoleApp1/DeliveryCalculator.cs
+++ ConsoleApp1/DeliveryCalculator.cs
@@ -10,7 +10,14 @@ namespace ConsoleApp1
     {
         public static void CalculateAndShow()
         {
-            
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введите стоймость заказа (руб.): ");
+            double cost = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите расстояние (км.): ");
+            double dist = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите время заказа (руб.): ");
+            double hour = double.Parse(Console.ReadLine());
+
         }
     }
 }
